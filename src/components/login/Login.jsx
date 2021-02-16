2e29eb544698880c5352c3aaee5609a11d49ff46
Arpit Rathi <galjonit2218@gmail.com>
2021-02-16 20:42:34 +0530

    removed tailwind support

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -82,19 +82,19 @@ const Login = () => {
     }
 
     return (
-        <div className="flex h-screen">
-            <div className="flex-1 flex justify-center item-center bg-blue-500">
-                <img src={landingPageIllustration} className="w-2/3" alt="APD" />
+        <div className="login">
+            <div className="login__left">
+                <img src={landingPageIllustration} alt="APD" />
             </div>
-            <div className="flex-1 flex flex-col justify-center items-center">
-                <h1 className="text-8xl m-4">APD</h1>
-                <div className="flex justify-start mx-auto text-2xl items-center">
-                    <span className={functionality == functionalConstants.login ? "login__tabs text-blue-600" : "login__tabs"} onClick={() => setFunctionality(functionalConstants.login)}>Login</span>
+            <div className="login__right">
+                <h1>APD</h1>
+                <div className="login__tabsContainer">
+                    <span className={functionality == functionalConstants.login ? "login__tabs selected" : "login__tabs"} onClick={() => setFunctionality(functionalConstants.login)}>Login</span>
                     <span>|</span>
-                    <span className={functionality == functionalConstants.signup ? "login__tabs text-blue-600" : "login__tabs"} onClick={() => setFunctionality(functionalConstants.signup)}>Signup</span>
+                    <span className={functionality == functionalConstants.signup ? "login__tabs selected" : "login__tabs"} onClick={() => setFunctionality(functionalConstants.signup)}>Signup</span>
                 </div>
-                <div className="flex flex-col">
-                    {error.length > 0 ? <span className="text-red-500 mx-auto my-2">{error}</span> : null}
+                <div className="login__form">
+                    {error.length > 0 ? <span className="login__error">{error}</span> : null}
                     {
                     functionality == functionalConstants.signup 
                         ? 
@@ -106,9 +106,9 @@ const Login = () => {
                     <input className="login__formElems" type="password" placeholder="Enter password" value={password} onChange={e => setPassword(e.target.value)} />
                     <button className="login__formElems login__formButton" onClick={() => authHandler()} disabled={loading} >{loading ? <span><CircularProgress size={20} /> Loading</span> : functionality}</button>
                 </div>
-                <span className="my-2">or</span>
-                <div className="login__google border border-blue-600 box-border my-4 rounded-lg flex items-center py-2 px-5 cursor-pointer" onClick={googleAuth}>
-                <img className="h-5 mr-3" src="https://img.favpng.com/7/1/24/google-logo-google-search-icon-png-favpng-DLXaPGArrFH6yJjYE8USnMuvX_t.jpg"></img>
+                <span>or</span>
+                <div className="login__google" onClick={googleAuth}>
+                <img src="https://img.favpng.com/7/1/24/google-logo-google-search-icon-png-favpng-DLXaPGArrFH6yJjYE8USnMuvX_t.jpg"></img>
                     <span>Continue with Google</span>
                 </div>
             </div>
